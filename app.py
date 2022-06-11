cdc7bc146415eaa7219c1403e6d0e533fbb43b57
Naval Sood <sonu.gupta@01s.in>
2022-06-11 15:41:29 +0530

    index show

--- app.py
+++ app.py
@@ -26,3 +26,9 @@ class Student(db.Model):
 
     def __repr__(self):
         return f'<Student {self.firstname}>'
+
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
